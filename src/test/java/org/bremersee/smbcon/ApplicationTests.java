5188e769eebeb9459671dffa19911b356dea2d0c
Christian Bremer <christian.bremer@cstx.de>
2019-01-10 18:26:50 +0100

    Version 1.1.0-SNAPSHOT

--- src/test/java/org/bremersee/smbcon/ApplicationTests.java
+++ src/test/java/org/bremersee/smbcon/ApplicationTests.java
@@ -1,18 +1,18 @@
 package org.bremersee.smbcon;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("in-memory")
 public class ApplicationTests {
 
-	@Ignore
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
