17bb2fe556c43e86cdc0c7c5220f0e665fe4ad30
Christian Bremer <christian.bremer@cstx.de>
2019-09-29 20:41:43 +0200

    Cache

--- src/main/java/org/bremersee/dccon/config/AsyncConfiguration.java
+++ src/main/java/org/bremersee/dccon/config/AsyncConfiguration.java
@@ -20,6 +20,7 @@ import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
@@ -29,6 +30,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
  */
 @Configuration
 @EnableAsync
+@EnableScheduling
 public class AsyncConfiguration implements AsyncConfigurer {
 
   @Override
